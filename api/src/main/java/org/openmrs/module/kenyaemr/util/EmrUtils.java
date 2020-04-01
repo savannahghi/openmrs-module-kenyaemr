65029699cff0685b94823735f2a399755462601a
ojwanganto <ojwangwachiaje@gmail.com>
2020-04-02 00:00:38 +0300

    fix concepts for comorbidities in CIF. hide location field in forms.add util method that returns user county

--- api/src/main/java/org/openmrs/module/kenyaemr/util/EmrUtils.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/util/EmrUtils.java
@@ -26,11 +26,14 @@ import org.openmrs.Order;
 import org.openmrs.OrderType;
 import org.openmrs.Patient;
 import org.openmrs.Person;
+import org.openmrs.PersonAttribute;
+import org.openmrs.PersonAttributeType;
 import org.openmrs.Provider;
 import org.openmrs.User;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.kenyaemr.Dictionary;
+import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -285,5 +288,13 @@ public class EmrUtils {
 				return Arrays.asList(countryNames.split(","));
 	}
 
+	public static String getUserCounty() {
+		PersonAttributeType userCountyType = Context.getPersonService().getPersonAttributeTypeByUuid(CommonMetadata._PersonAttributeType.COUNTY);
+		PersonAttribute userCounty = Context.getAuthenticatedUser().getPerson().getAttribute(userCountyType);
+		if (userCounty != null) {
+			return null;
+		}
+		return userCounty.getValue();
+	}
 
 }
