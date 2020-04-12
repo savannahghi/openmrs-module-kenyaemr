5377aa51f43bcf3ae39477f810f93a484517b9ec
ojwanganto <ojwangwachiaje@gmail.com>
2020-04-12 23:58:43 +0300

    added payload for ESRIE dashboard indicators

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/report/EsriDashboardViewFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/report/EsriDashboardViewFragmentController.java
@@ -9,11 +9,13 @@
  */
 package org.openmrs.module.kenyaemr.fragment.controller.report;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
 import org.openmrs.GlobalProperty;
 import org.openmrs.Location;
@@ -59,8 +61,10 @@ import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Page for viewing ADX message generated for DHIS2
@@ -109,6 +113,85 @@ public class EsriDashboardViewFragmentController {
         model.addAttribute("serverAddressLength", SERVER_ADDRESS.length());
     }
 
+    public String render(ReportData reportData) throws IOException {
+
+
+        Date reportDate = (Date) reportData.getContext().getParameterValue("startDate");
+        Date endDate = (Date) reportData.getContext().getParameterValue("endDate");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ObjectNode payloadNode = mapper.createObjectNode();
+        ArrayNode counties = mapper.createArrayNode();
+        for (String dsKey : reportData.getDataSets().keySet()) {
+
+            DataSet dataset = reportData.getDataSets().get(dsKey);
+            List<DataSetColumn> columns = dataset.getMetaData().getColumns();
+            for (DataSetRow row : dataset) {
+                for (DataSetColumn column : columns) {
+                    String name = column.getName();
+                    String label = column.getLabel();
+                    Object value = row.getColumnValue(column);
+                    payloadNode.put(formatIndicatorName(name), value.toString());
+                }
+            }
+        }
+
+        String qry = "select e.patient_id, county, recently_travelled\n" +
+                "from kenyaemr_etl.etl_covid_19_enrolment e\n" +
+                "inner join kenyaemr_etl.etl_laboratory_extract l on l.patient_id=e.patient_id and l.result = 703\n" +
+                "where e.voided=0;";
+
+        Map<String, ObjectNode> countyList = new HashMap<String, ObjectNode>();
+        List<List<Object>> activeList = Context.getAdministrationService().executeSQL(qry, true);
+        if (!activeList.isEmpty()) {
+            for (List<Object> res : activeList) {
+                Integer patientId = (Integer) res.get(0);
+                String county = (String) res.get(1);
+                String traveled = (String) res.get(2);
+
+                if (StringUtils.isBlank(county)) {
+                    continue;
+                }
+
+                if (countyList.containsKey(county)) {
+                    ObjectNode details = countyList.get(county);
+                    details.put("confirmed", details.get("confirmed").getIntValue() + 1);
+                    if (traveled.equals("Yes")) {
+                        details.put("imported", details.get("imported").getIntValue() + 1);
+                    } else {
+                        details.put("local", details.get("local").getIntValue() + 1);
+                    }
+                } else {
+                    ObjectNode countyDetails = mapper.createObjectNode();
+                    countyDetails.put("confirmed", 1);
+                    if (traveled.equals("Yes")) {
+                        countyDetails.put("imported", 1);
+                        countyDetails.put("local", 0);
+                    } else {
+                        countyDetails.put("imported", 0);
+                        countyDetails.put("local", 1);
+                    }
+                    countyList.put(county, countyDetails);
+                }
+            }
+        }
+
+        if (!countyList.keySet().isEmpty()) {
+            for (Map.Entry<String, ObjectNode> entry : countyList.entrySet()) {
+                String countyName = entry.getKey();
+                ObjectNode stats = entry.getValue();
+                ObjectNode countyObj = mapper.createObjectNode();
+                countyObj.put(countyName, stats);
+                counties.add(countyObj);
+            }
+        }
+        payloadNode.put("county", counties);
+        Object json = mapper.readValue(payloadNode, Object.class);
+        String indented = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        return indented;
+    }
+
     private String formatIndicatorName(String name) {
 
         String arrName[] = name.split("-"); // should come in the form of Newly Diagnosed-01
@@ -153,32 +236,7 @@ public class EsriDashboardViewFragmentController {
         }
         return resultingString;
     }
-    public String render(ReportData reportData) throws IOException {
 
-
-        Date reportDate = (Date) reportData.getContext().getParameterValue("startDate");
-        Date endDate = (Date) reportData.getContext().getParameterValue("endDate");
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        ObjectNode payloadNode = mapper.createObjectNode();
-        for (String dsKey : reportData.getDataSets().keySet()) {
-
-            DataSet dataset = reportData.getDataSets().get(dsKey);
-            List<DataSetColumn> columns = dataset.getMetaData().getColumns();
-            for (DataSetRow row : dataset) {
-                for (DataSetColumn column : columns) {
-                    String name = column.getName();
-                    String label = column.getLabel();
-                    Object value = row.getColumnValue(column);
-                    payloadNode.put(formatIndicatorName(name), value.toString());
-                }
-            }
-        }
-        Object json = mapper.readValue(payloadNode, Object.class);
-        String indented = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
-        return indented;
-    }
     public SimpleObject buildXmlDocument(@RequestParam("request") ReportRequest reportRequest,
                                           @RequestParam("returnUrl") String returnUrl,
                                           @SpringBean ReportService reportService) throws ParserConfigurationException, IOException, TransformerException {
