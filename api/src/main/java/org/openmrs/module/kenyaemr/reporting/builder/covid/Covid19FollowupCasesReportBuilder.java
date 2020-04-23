92c706ba7e32c56da2ee0ffe83d25806ffbf23c5
njorocs <njorocs@gmail.com>
2020-04-24 00:22:38 +0300

    Added contact idicators report

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/covid/Covid19FollowupCasesReportBuilder.java
@@ -0,0 +1,106 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr.reporting.builder.covid;
+
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.PersonAttributeType;
+import org.openmrs.module.kenyacore.report.HybridReportDescriptor;
+import org.openmrs.module.kenyacore.report.ReportDescriptor;
+import org.openmrs.module.kenyacore.report.ReportUtils;
+import org.openmrs.module.kenyacore.report.builder.AbstractHybridReportBuilder;
+import org.openmrs.module.kenyacore.report.builder.Builds;
+import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
+import org.openmrs.module.kenyaemr.reporting.cohort.definition.covid.ConfirmedCovid19PositivesCohortDefinition;
+import org.openmrs.module.kenyaemr.reporting.cohort.definition.covid.Covid19FollowupCasesCohortDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.IdentifierConverter;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.covid.*;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.data.DataDefinition;
+import org.openmrs.module.reporting.data.converter.BirthdateConverter;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+import org.openmrs.module.reporting.data.converter.DateConverter;
+import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.*;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Builds({"kenyaemr.common.report.contactTracing"})
+public class Covid19FollowupCasesReportBuilder extends AbstractHybridReportBuilder {
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    @Override
+    protected Mapped<CohortDefinition> buildCohort(HybridReportDescriptor descriptor, PatientDataSetDefinition dsd) {
+        return covidContactsFollowupCohort();
+    }
+
+    protected Mapped<CohortDefinition> covidContactsFollowupCohort() {
+        CohortDefinition cd = new Covid19FollowupCasesCohortDefinition();
+        cd.setName("covidCases");
+        return ReportUtils.map(cd, "");
+    }
+
+    @Override
+    protected List<Mapped<DataSetDefinition>> buildDataSets(ReportDescriptor descriptor, ReportDefinition report) {
+
+        PatientDataSetDefinition covidCases = covidContactsDataSetDefinition();
+        covidCases.addRowFilter(covidContactsFollowupCohort());
+        DataSetDefinition covidCasesDSD = covidCases;
+
+        return Arrays.asList(
+                ReportUtils.map(covidCasesDSD, "")
+        );
+    }
+
+    protected PatientDataSetDefinition covidContactsDataSetDefinition() {
+
+        PatientIdentifierType nationalIdType = MetadataUtils.existing(PatientIdentifierType.class, CommonMetadata._PatientIdentifierType.NATIONAL_ID);
+        PatientIdentifierType passportNumberType = MetadataUtils.existing(PatientIdentifierType.class, CommonMetadata._PatientIdentifierType.PASSPORT_NUMBER);
+        DataDefinition natIdDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(nationalIdType.getName(), nationalIdType), new IdentifierConverter());
+        DataDefinition passportDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(passportNumberType.getName(), passportNumberType), new IdentifierConverter());
+        PersonAttributeType phoneNumber = MetadataUtils.existing(PersonAttributeType.class, CommonMetadata._PersonAttributeType.TELEPHONE_CONTACT);
+        String DATE_FORMAT = "dd/MM/yyyy";
+        PatientDataSetDefinition dsd = new PatientDataSetDefinition("covidCases");
+
+        PatientIdentifierType upn = MetadataUtils.existing(PatientIdentifierType.class, CommonMetadata._PatientIdentifierType.NATIONAL_ID);
+        DataConverter identifierFormatter = new ObjectFormatter("{identifier}");
+        DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(upn.getName(), upn), identifierFormatter);
+
+        DataConverter nameFormatter = new ObjectFormatter("{familyName}, {givenName} {middleName}");
+        DataDefinition nameDef = new ConvertedPersonDataDefinition("name", new PreferredNameDataDefinition(), nameFormatter);
+        dsd.addColumn("id", new PersonIdDataDefinition(), "");
+
+        dsd.addColumn("Case Name", nameDef, "");
+        //dsd.addColumn("Case", nameDef, "");
+        dsd.addColumn("Contacts identified",new ContactsIdentifiedDataDefinition(), "");
+        dsd.addColumn("Contacts converted to cases",new ContactsConvertedToCasesDataDefinition(), "");
+        dsd.addColumn("Contacts on followup",new ContactsOnFollowUpDataDefinition(), "");
+        dsd.addColumn("Contacted today",new NumberContactedTodayDataDefinition(), "");
+        dsd.addColumn("Not contacted today",new NumberNotContactedTodayDataDefinition(), "");
+        //dsd.addColumn("Response rate",new ResponseRateDataDefinition(), "");
+        dsd.addColumn("Developed signs and symptoms",new ContactsDevelopedSignsAndSymptomsDataDefinition(), "");
+        dsd.addColumn("Positive lab results",new ContactsWithPositiveLabResultsDataDefinition(), "");
+        dsd.addColumn("County",new CountyDataDefinition(), "");
+        //dsd.addColumn("Comments",new CountyDataDefinition(), "");
+        //dsd.addColumn("Status of case Contact tracing",new CountyDataDefinition(), "");
+        return dsd;
+    }
+
+}
+
---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/covid/ContactsDevelopedSignsAndSymptomsDataEvaluator.java
@@ -0,0 +1,61 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr.reporting.data.converter.definition.evaluator.covid;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.covid.ContactsDevelopedSignsAndSymptomsDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Evaluates Contacts Developed Signs And Symptoms data definition
+ */
+@Handler(supports= ContactsDevelopedSignsAndSymptomsDataDefinition.class, order=50)
+public class ContactsDevelopedSignsAndSymptomsDataEvaluator implements PersonDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+
+        String qry = "select f.case_patient,count(f.patient_id)\n" +
+                "from (\n" +
+                "     select c.patient_id, self_q.patient_id selfQ,c.patient_related_to case_patient, coalesce(self_q.cough,gov_q.cough) cough, coalesce(self_q.fever, gov_q.fever) fever, coalesce(self_q.difficulty_breathing,gov_q.difficulty_breathing) difficulty_breathing,\n" +
+                "            coalesce(self_q.sore_throat,gov_q.sore_throat) sorethroat,\n" +
+                "            min(self_q.visit_date) first_self_q_followup_date, gov_q.patient_id govQ, min(gov_q.visit_date) first_gov_quarantine_date\n" +
+                "     from openmrs.kenyaemr_hiv_testing_patient_contact c\n" +
+                "            inner join kenyaemr_etl.etl_covid_19_enrolment e on e.patient_id = c.patient_related_to\n" +
+                "            left join kenyaemr_etl.etl_contact_tracing_followup self_q on self_q.patient_id = c.patient_id\n" +
+                "            left join kenyaemr_etl.etl_covid_quarantine_followup gov_q on gov_q.patient_id = c.patient_id\n" +
+                "     where c.voided=0 and e.voided = 0\n" +
+                "     ) f where (f.fever = 'Yes' or f.cough = 'Yes' or f.difficulty_breathing = 'Yes' or f.sorethroat = 'Yes') and\n" +
+                "    (selfQ is not null and datediff(curdate(),first_self_q_followup_date) < 14) or (govQ is not null and datediff(curdate(), first_gov_quarantine_date) < 14) group by f.case_patient;";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        Date startDate = (Date)context.getParameterValue("startDate");
+        Date endDate = (Date)context.getParameterValue("endDate");
+        queryBuilder.addParameter("endDate", endDate);
+        queryBuilder.addParameter("startDate", startDate);
+        queryBuilder.append(qry);
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+}
